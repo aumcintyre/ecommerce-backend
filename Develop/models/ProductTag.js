7d39dc9124977dd7bb52471f0a4f6333668e5bee
aumcintyre <aumcintyre@gmail.com>
2022-10-16 09:38:16 -0500

    product tag model built

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
@@ -10,11 +10,33 @@ ProductTag.init(
 
     //ID column
 
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
     //product_id column
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
 
     //tag_id column
 
-    
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
+
   },
   {
     sequelize,
