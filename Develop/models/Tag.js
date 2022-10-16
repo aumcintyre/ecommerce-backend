f9bae597af7e07f8b0e093619acbbb7ecfc117ad
aumcintyre <aumcintyre@gmail.com>
2022-10-16 09:40:52 -0500

    tag model built

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -2,15 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
     // define columns
 
     //ID Column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
 
     //tag_name column
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
