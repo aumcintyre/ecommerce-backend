c108959467768eacb130a5cc4fa55572fddf4008
aumcintyre <aumcintyre@gmail.com>
2022-10-17 13:00:54 -0500

    product post route throwing 400

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,6 +1,8 @@
+-- Active: 1665083799973@@127.0.0.1@3306@library_db
 -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
