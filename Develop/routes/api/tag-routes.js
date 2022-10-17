9923a94d6d63d98cb00209ec5d09f4fb40263e20
aumcintyre <aumcintyre@gmail.com>
2022-10-17 17:44:32 -0500

    tag routes built but not yet tested

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -59,10 +59,38 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if (!tagData[0]){
+      res.status(404).json('No tag found with this ID!')
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(tagData => {
+    if(!tagData){
+      res.status(404).json('No tag found with this ID!')
+    }
+    res.json(tagData);
+  })
+  .catch(err => {
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
