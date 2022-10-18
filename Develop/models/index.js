a52bc5ee9e4d5718662040685064bf34e8d31bac
aumcintyre <aumcintyre@gmail.com>
2022-10-18 16:41:21 -0500

    removed unnecessary quotations on model names

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -8,28 +8,27 @@ const ProductTag = require('./ProductTag');
 
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 })
 
 // Categories have many Products
 
 Category.hasMany(Product, {
 foreignKey: 'category_id',
-onDelete: 'SET NULL',
 })
 
 // Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
-  through: 'ProductTag',
+  through: ProductTag,
   foreignKey: 'product_id',
-  onDelete: 'SET NULL'
 })
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  through: 'ProductTag',
-  foreignKey: 'tag_id'
+  through: ProductTag,
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
